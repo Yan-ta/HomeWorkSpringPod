e0f7d2450f9c239f6449e9ba08a0a73eb8175db4
Yan-ta <yatashe@yandex.ru>
2022-04-07 18:52:18 +0300

    Initial Commit

---
+++ HomeWorkSpringPod/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HomeWorkSpringPod
+//
+//  Created by Ян Таше on 07.04.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
